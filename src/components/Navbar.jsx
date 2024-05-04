26decfb9d7d2c922f706e425b5d226002ec98db2
J4jatin <117545430+J4jatin@users.noreply.github.com>
2024-05-05 03:43:31 +0530

    next_changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { NavLink, Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav id="navbar Sticky" className="navbar">
+    <nav id="navbarSticky" className="navbar">
       <div className="navbar-logo">
         <NavLink to="/">
           <img src="" alt="image1" />
